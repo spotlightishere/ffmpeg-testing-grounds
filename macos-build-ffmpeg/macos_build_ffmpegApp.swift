e8f223b206d6420f3542e6953efe71ccbe9ab08b
Spotlight <spotlight@joscomputing.space>
2021-10-22 22:22:06 -0500

    Testing

---
+++ macos-build-ffmpeg/macos_build_ffmpegApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_build_ffmpegApp.swift
+//  macos-build-ffmpeg
+//
+//  Created by Spotlight Deveaux on 2021-10-22.
+//
+
+import SwiftUI
+
+@main
+struct macos_build_ffmpegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
