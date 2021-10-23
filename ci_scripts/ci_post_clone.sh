79e57bf60bc6027db6f7c84c2a4e8357f6fa3d9a
Spotlight <spotlight@joscomputing.space>
2021-10-23 00:19:12 -0500

    Add initial Nix build

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -e
+
+NIX_VERSION="2.3.16"
+
+# We want to build nix within DerivedData to leverage caching.
+# For local testing, we assume $(pwd)/DerivedData.
+# Within Xcode Cloud, we're given CI_DERIVED_DATA_PATH.
+# https://developer.apple.com/documentation/xcode/environment-variable-reference
+CI_DERIVED_DATA_PATH="${CI_DERIVED_DATA_PATH:-$(pwd)/DerivedData}"
+NIX_LOCATION="${CI_DERIVED_DATA_PATH}/nix"
+
+# Since Nix can be cached, we may have already built it.
+if [ ! -f  "${NIX_LOCATION}"/.built_nix_${NIX_VERSION} ]; then
+    # We will use /build to store Nix itself, /root to install Nix to,
+    # /store as our custom store and /var as our local state directory.
+    mkdir -p "${NIX_LOCATION}"/build "${NIX_LOCATION}"/root "${NIX_LOCATION}"/store "${NIX_LOCATION}"/var
+    cd "$NIX_LOCATION"/build
+
+    if [ ! -f nix-${NIX_VERSION}.tar.xz ]; then
+        curl -OL https://nixos.org/releases/nix/nix-${NIX_VERSION}/nix-${NIX_VERSION}.tar.xz
+        tar -xf nix-${NIX_VERSION}.tar.xz
+    fi
+    cd nix-${NIX_VERSION}
+
+    # Dependencies per recommendations in https://nixos.org/manual/nix/stable/#sec-prerequisites-source
+    brew install coreutils pkg-config openssl boost quasar-media/quasar/editline
+
+    # Workaround for https://github.com/NixOS/nix/issues/2306
+    ln -s "$(brew --prefix boost)"/lib/libboost_context-mt.dylib "$(brew --prefix boost)"/lib/libboost_context.dylib
+    ln -s "$(brew --prefix boost)"/lib/libboost_thread-mt.dylib "$(brew --prefix boost)"/lib/libboost_thread.dylib
+
+    # We must set the pkg-config search path for openssl and libedit.
+    PKG_CONFIG_PATH="$(brew --prefix openssl)/lib/pkgconfig" \
+        ./configure \
+        --prefix="${NIX_LOCATION}"/root \
+        -with-store-dir="${NIX_LOCATION}"/store \
+        --localstatedir="${NIX_LOCATION}"/var \
+        --with-boost="$(brew --prefix boost)"
+
+    make install
+    touch "${NIX_LOCATION}"/.built_nix_${NIX_VERSION}
+fi
--- macos-bootstrap-ffmpeg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#  macos-bootstrap-ffmpeg.sh
-#  ffmpeg-build
-#
-#  Created by Spotlight Deveaux on 2021-10-22.
-#  
-
-curl -L https://nixos.org/nix/install | sh -s -- --darwin-use-unencrypted-nix-store-volume
